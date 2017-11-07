4d8783983d125a093fd4027bc330e2e3918f37e9
Yusuke Sangenya <longinus.eva@gmail.com>
2017-11-07 19:36:59 +0900

    よしなにCSVをtabularに変換してくれるくんを作成

--- 2017_10_13/convert_csv.rb
+++ 2017_10_13/convert_csv.rb
@@ -13,11 +13,14 @@ latex_tabulars = all_rows.each_slice(rows_count_for_each_tabular).map do |rows|
     headers.map { |header| row[header] }.join(' & ')
   end.join(" \\\\ \\hline \n")
   <<-TABULAR
-  \\begin{tabular}
+  \\subfloat{
+  \\begin{tabular}{|#{"c|" * headers.count}}
+    \\hline
     #{headers.join(' & ')} \\\\ \\hline \\hline
-    #{latex_rows}
+    #{latex_rows} \\\\ \\hline
   \\end{tabular}
+  }
   TABULAR
-end.join("\n")
+end.join
 
 puts latex_tabulars
